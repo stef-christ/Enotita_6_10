ed6ff579d3a02695a1d3e86814f8921e6e1868dd
stef-christ <stefanosc@yahoo.com>
2019-07-11 12:42:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/enotita_6_10/Settings.java
@@ -0,0 +1,66 @@
+package com.example.enotita_6_10;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_settings);
+        item.setVisible(false);
+        this.invalidateOptionsMenu();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_main:
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                this.startActivity(intent);
+                break;
+            case R.id.action_help:
+                Intent intent1 = new Intent(Settings.this, Help.class);
+                this.startActivity(intent1);
+                break;
+            case R.id.action_about:
+                Intent intent2 = new Intent(Settings.this, About.class);
+                this.startActivity(intent2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+}
